858581f3f05518aeedcf7a5a51333931c394c0d1
XBlueSky <30610447+XBlueSky@users.noreply.github.com>
2017-08-12 00:37:26 +0800

    Add files via upload

---
+++ release/moc_widget.cpp
@@ -0,0 +1,288 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../VCG_practice/widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_ClassWizard_t {
+    QByteArrayData data[1];
+    char stringdata0[12];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ClassWizard_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ClassWizard_t qt_meta_stringdata_ClassWizard = {
+    {
+QT_MOC_LITERAL(0, 0, 11) // "ClassWizard"
+
+    },
+    "ClassWizard"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ClassWizard[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void ClassWizard::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObject ClassWizard::staticMetaObject = {
+    { &QWizard::staticMetaObject, qt_meta_stringdata_ClassWizard.data,
+      qt_meta_data_ClassWizard,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *ClassWizard::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ClassWizard::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ClassWizard.stringdata0))
+        return static_cast<void*>(const_cast< ClassWizard*>(this));
+    return QWizard::qt_metacast(_clname);
+}
+
+int ClassWizard::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWizard::qt_metacall(_c, _id, _a);
+    return _id;
+}
+struct qt_meta_stringdata_IntroPage_t {
+    QByteArrayData data[1];
+    char stringdata0[10];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_IntroPage_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_IntroPage_t qt_meta_stringdata_IntroPage = {
+    {
+QT_MOC_LITERAL(0, 0, 9) // "IntroPage"
+
+    },
+    "IntroPage"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_IntroPage[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void IntroPage::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObject IntroPage::staticMetaObject = {
+    { &QWizardPage::staticMetaObject, qt_meta_stringdata_IntroPage.data,
+      qt_meta_data_IntroPage,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *IntroPage::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *IntroPage::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_IntroPage.stringdata0))
+        return static_cast<void*>(const_cast< IntroPage*>(this));
+    return QWizardPage::qt_metacast(_clname);
+}
+
+int IntroPage::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWizardPage::qt_metacall(_c, _id, _a);
+    return _id;
+}
+struct qt_meta_stringdata_RegistrationPage_t {
+    QByteArrayData data[1];
+    char stringdata0[17];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_RegistrationPage_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_RegistrationPage_t qt_meta_stringdata_RegistrationPage = {
+    {
+QT_MOC_LITERAL(0, 0, 16) // "RegistrationPage"
+
+    },
+    "RegistrationPage"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_RegistrationPage[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void RegistrationPage::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObject RegistrationPage::staticMetaObject = {
+    { &QWizardPage::staticMetaObject, qt_meta_stringdata_RegistrationPage.data,
+      qt_meta_data_RegistrationPage,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *RegistrationPage::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *RegistrationPage::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_RegistrationPage.stringdata0))
+        return static_cast<void*>(const_cast< RegistrationPage*>(this));
+    return QWizardPage::qt_metacast(_clname);
+}
+
+int RegistrationPage::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWizardPage::qt_metacall(_c, _id, _a);
+    return _id;
+}
+struct qt_meta_stringdata_ConclusionPage_t {
+    QByteArrayData data[1];
+    char stringdata0[15];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ConclusionPage_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ConclusionPage_t qt_meta_stringdata_ConclusionPage = {
+    {
+QT_MOC_LITERAL(0, 0, 14) // "ConclusionPage"
+
+    },
+    "ConclusionPage"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ConclusionPage[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       0,    0, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+       0        // eod
+};
+
+void ConclusionPage::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+}
+
+const QMetaObject ConclusionPage::staticMetaObject = {
+    { &QWizardPage::staticMetaObject, qt_meta_stringdata_ConclusionPage.data,
+      qt_meta_data_ConclusionPage,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *ConclusionPage::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ConclusionPage::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ConclusionPage.stringdata0))
+        return static_cast<void*>(const_cast< ConclusionPage*>(this));
+    return QWizardPage::qt_metacast(_clname);
+}
+
+int ConclusionPage::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWizardPage::qt_metacall(_c, _id, _a);
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
